67849820208b64be51fe033504eb839d7f2659e8
nikhil027 <nikhil.sim@gmail.com>
2018-12-28 09:51:31 +0530

    commit 4

---
+++ Javascript/day25-dom-traversing/movie.js
@@ -0,0 +1,29 @@
+const movie = {
+    name: "Buddivantha",
+    summary: "This is some stupid text, it is supposed to be about the movie stated above but Im in no mood to type all those, please dont read aloud Thanks!!!",
+    directors: [
+                    {
+                        name:"Uppi",
+                        profileinfo:'https://www.imdb.com/name/nm1962192/?ref_=tt_ov_dr'
+                    }
+            ],
+    actors: [
+                {
+                    name: "Naanu",
+                    age: 52,
+                    starRating: 4 
+                },
+                {
+                    name: "Nanavanalla",
+                    age: 45,
+                    starRating: 5
+                },
+                {
+                    name: "Neenu",
+                    age: 25,
+                    starRating: 3
+                }
+            ]
+}
+
+console.log(movie)
